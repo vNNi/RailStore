872af55d2ac63bca5d513152e25c2f31081dd899
Vinicius Komninakis <komninakis@porquenao.mobi>
2019-04-11 14:03:05 -0300

    controller to create new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,13 @@ class ProductsController < ApplicationController
     def index
         @products = Produto.order :name
     end
+    def create 
+        products = params.require(:product).permit(:name, :description, :price, :quantity)
+        if(products)
+            Produto.create(products);
+        elsif
+            redirect_to('/products/new');
+        end
+        redirect_to('/');
+    end
 end
