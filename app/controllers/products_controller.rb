e36691ab15fe04500ccb41e54c980a1a7ef2631e
Vinicius Komninakis <komninakis@porquenao.mobi>
2019-04-11 18:02:42 -0300

    delete feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,10 +5,19 @@ class ProductsController < ApplicationController
     def create 
         products = params.require(:product).permit(:name, :description, :price, :quantity)
         if(products)
-            Produto.create(products);
+            Produto.create(products)
         elsif
-            redirect_to('/products/new');
+            redirect_to('/products/new')
         end
         redirect_to('/');
+    end
+    def destroy
+        id = params[:id].to_i()
+        if(id)
+            Produto.destroy(id)
+            redirect_to root_path
+        elsif
+            redirect_to root_path
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: "products#index"
   get "/products/new", to: "products#new"
   post "/products", to: "products#create"
+  delete "/products/:id", to: "products#destroy", as: :produto
+  get "/products/search", to: "products#search"
 end
