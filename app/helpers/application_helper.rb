8ac1ec4b252ba8b431c2a74200c1a324d1dd886c
Vinicius Komninakis <komninakis@porquenao.mobi>
2019-04-12 14:41:50 -0300

    add flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def flash_message
+        message = ""
+        [:success, :info, :warning, :danger].each {|type|
+            if flash[type]
+                message = "<div class=\"alert alert-#{type}\" role=\"alert\">#{flash[type]}</div>".html_safe()
+            end
+        }
+            message
+    end
 end
